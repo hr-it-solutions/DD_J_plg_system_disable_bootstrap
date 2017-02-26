b51713d59b5c440e57c84a635f66efc4985b1b1f
Florian Häusler <florian@didldu.com>
2017-02-26 16:49:11 +0100

    remove custom snipped option implemented

--- dd_disable_bootstrap.php
+++ dd_disable_bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version    1-3-1-0 // Y-m-d 2017-02-26
+ * @version    1-3-1-1 // Y-m-d 2017-02-26
  * @author     HR IT-Solutions Florian Häusler https://www.hr-it-solutions.com
  * @copyright  Copyright (C) 2011 - 2016 Didldu e.K. | HR IT-Solutions
  * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
@@ -44,15 +44,21 @@ class plgSystemDD_Disable_Bootstrap extends JPlugin
 		if ($this->app instanceof JApplicationSite)
 		{
 			// check plugin parameter to replace
-			if ($this->params->get('toreplace') !== '{"html": true,"container": "body"}'){
+			if ($this->params->get('remove_hastooltip_class',1) AND
+				$this->params->get('toreplace') !== '{"html": true,"container": "body"}'){
+
 				$value = array("(",")"); // Securety fix, remove opening braces
 				$toreplace = str_replace($value,"",$this->params->get('toreplace'));
-			} else {
-				$toreplace = $this->behavior_tooltip;
-			}
 
-			// Remove bootstrap associated .tooltip global from <head> which is added by JHtml::_('behavior.tooltip');
-			$html = preg_replace("/jQuery(.+)\.tooltip\(.+\)\;/i", "<!-- Removed tooltip -->", $this->app->getBody());
+				// Remove bootstrap associated .tooltip global from <head> which is added by JHtml::_('behavior.tooltip');
+				$html = str_replace('jQuery(\'.hasTooltip\').tooltip(' . $toreplace . ');', '', $this->app->getBody());
+
+			} else {
+
+				// Remove bootstrap associated .tooltip global from <head> which is added by JHtml::_('behavior.tooltip');
+				$html = preg_replace("/jQuery(.+)\.tooltip\(.+\)\;/i", "<!-- Removed tooltip -->", $this->app->getBody());
+
+			}
 
 			$this->app->setBody($html);
 		}
