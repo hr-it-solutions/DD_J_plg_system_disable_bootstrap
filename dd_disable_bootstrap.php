b98336909e80f51d73c092e99042d1cae8e0eaad
Florian Häusler <florian@didldu.com>
2017-04-28 17:14:34 +0200

    New legal notes / copyright header Improved code style phpcs Joomla close issue #21 replace str_replace with preg_replace

--- dd_disable_bootstrap.php
+++ dd_disable_bootstrap.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * @version    1-3-1-1 // Y-m-d 2017-02-26
- * @author     HR IT-Solutions Florian Häusler https://www.hr-it-solutions.com
- * @copyright  Copyright (C) 2011 - 2016 Didldu e.K. | HR IT-Solutions
+ * @package    DD_Disable_Bootstrap
+ *
+ * @author     HR IT-Solutions Florian Häusler <info@hr-it-solutions.com>
+ * @copyright  Copyright (C) 2011 - 2017 Didldu e.K. | HR IT-Solutions
  * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
  **/
 
@@ -20,53 +21,55 @@ class plgSystemDD_Disable_Bootstrap extends JPlugin
 	protected $app;
 
 	/**
-	 * @var string
-	 * Joomla 3.x
-	 * JHtml::_('behavior.tooltip') JavaScript jQuery snipped
+	 * onBeforeCompileHead
+	 *
+	 * @since Version 1.0.0.0
 	 */
-	private $behavior_tooltip = '{"html": true,"container": "body"}';
-	
 	public function onBeforeCompileHead()
 	{
 		// Front end
 		if ($this->app instanceof JApplicationSite)
 		{
 			$doc = JFactory::getDocument();
+
 			// Remove default bootstrap
 			unset($doc->_scripts[JURI::root(true) . '/media/jui/js/bootstrap.min.js']);
 		}
 	}
 
+	/**
+	 * onAfterRender
+	 *
+	 * @since Version 1.0.0.0
+	 */
 	public function onAfterRender()
 	{
-
 		// Front end
 		if ($this->app instanceof JApplicationSite)
 		{
-			// check plugin parameter to replace
-			if ($this->params->get('remove_hastooltip_class',1) AND
-				$this->params->get('toreplace') !== '{"html": true,"container": "body"}'){
+			// Check plugin parameter to replace
+			if ($this->params->get('remove_hastooltip_class', 1)
+				&& $this->params->get('toreplace') !== '{"html": true,"container": "body"}')
+			{
+				// Securety fix, remove braces
+				$value = array("(",")");
 
-				$value = array("(",")"); // Securety fix, remove opening braces
-				$toreplace = str_replace($value,"",$this->params->get('toreplace'));
+				$toreplace = str_replace($value, "", $this->params->get('toreplace'));
 
 				// Remove bootstrap associated .tooltip global from <head> which is added by JHtml::_('behavior.tooltip');
 				$html = str_replace('jQuery(\'.hasTooltip\').tooltip(' . $toreplace . ');', '', $this->app->getBody());
-
 			}
 			else
 			{
-
 				// Remove bootstrap associated .tooltip global from <head> which is added by JHtml::_('behavior.tooltip');
 				$html = preg_replace("/jQuery(.+)\.tooltip\(.+\)\;/i", "<!-- Removed tooltip -->", $this->app->getBody());
-
 			}
 
-			// check plugin parameter to remove hasTooltip CSS class
-			if ($this->params->get('remove_hastooltip_class',1))
+			// Check plugin parameter to remove hasTooltip CSS class
+			if ($this->params->get('remove_hastooltip_class', 1))
 			{
 				// Try to remove all associated JS CSS .tooltip classes to avoid TypeError: $(...).tooltip is not a function.
-				$html = str_replace(' hasTooltip"', ' "', $html); // todo-DD! #21 replace str_replace with preg_replace regex to remove only hasTooltip in CSSclass attributes, but not other class names
+				$html = preg_replace("/\shasTooltip\"/", ' "', $html);
 			}
 
 			$this->app->setBody($html);
